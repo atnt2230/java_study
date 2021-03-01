3d38e7c4fadaf07ebba3072a11e2a34a9227361d
atnt2230 <ws@ws-ui-MacBookPro.local>
2021-03-01 22:48:41 +0900

    test git commit

---
+++ test.js
@@ -0,0 +1,34 @@
+let test1 = "hello";
+console.log(test1);
+console.log("change");
+test1 = "bye";
+console.log(test1);
+
+test();
+
+function test() {
+    console.log("함수 호출");
+}
+
+
+
+var v = "1";
+
+console.log(v);
+
+v = 2;
+
+console.log(v);
+
+k = 1;
+console.log("k :" +k );
+var k = "2";
+
+
+function test12() {
+    console.log("function call");
+}
+
+test12();
+
+// run -> cotrol option n
